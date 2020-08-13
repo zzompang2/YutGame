cebcada49459acc0e48cc1b327dbf89a58075a24
함창수 <ham@hamchangsuui-MacBook-Pro.local>
2020-08-14 04:27:57 +0900

    throw yut

---
+++ Assets/Script/YutCheckZone.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class YutCheckZone : MonoBehaviour
+{
+  Vector3 yutVelocity;
+  public Text scoreText;
+  public YutScript yut1, yut2, yut3, yut4;
+  int[] yutResult = new int[4];
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+      
+  }
+
+  private void FixedUpdate() {
+    yutVelocity = YutScript.yutVelocity;
+
+    // if(Input.GetKeyDown (KeyCode.Space)){
+    //   score = 0;
+    // }
+  }
+
+  private void OnTriggerStay(Collider collider) {
+    if(yutVelocity.x == 0f && yutVelocity.y == 0f && yutVelocity.z == 0f){
+      switch(collider.gameObject.name){
+        case "Side_top":
+          Debug.Log("side top");
+          yutResult[collider.gameObject.GetComponentInParent<YutScript>().yutId] = 1;
+          //yutResult[collider.gameObject.GetComponentInParent<YutScript>().yutId] = 1;
+          break;
+        case "Side_bottom":
+          Debug.Log("side bottom");
+          yutResult[collider.gameObject.GetComponentInParent<YutScript>().yutId] = 0;
+          break;
+      }
+      scoreText.text = ""+yutResult[0]+yutResult[1]+yutResult[2]+yutResult[3];
+    }
+  }
+}
---
+++ Assets/Script/YutScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YutScript : MonoBehaviour
+{
+  public Rigidbody rigid;
+  public static Vector3 yutVelocity;
+  public int yutId;
+  // Start is called before the first frame update
+  void Start()
+  {
+    // rigid = GetComponent<Rigidbody>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    yutVelocity = rigid.velocity;
+
+    if(Input.GetKeyDown (KeyCode.Space)){
+      rigid.velocity = Vector3.zero; 
+      rigid.angularVelocity = Vector3.zero; 
+
+      float posX = Random.Range(-3, 3);
+      float posZ = Random.Range(-3, 3);
+      transform.position = new Vector3(posX, 3, posZ);
+
+      //transform.rotation = Quaternion.identity;
+      rigid.AddForce(new Vector3(0,500,0)); // 위로 던지기
+      
+      float dirX = Random.Range(300,700);
+      // float dirY = Random.Range(200,500);
+      // float dirZ = Random.Range(200,500);
+      rigid.AddTorque(dirX, 10, 10);  // 각도 랜덤 설정 
+    }
+  }
+    
+}
